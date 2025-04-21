8b060439c71d58e237322578db229ecf0f6c5bde
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2025-04-21 06:20:02 -0400

    Init JpaOverRest

---
+++ sample7_jpa_over_rest/src/main/java/com/example/jpa_over_rest/jpa_over_rest/JpaOverRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_over_rest.jpa_over_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOverRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaOverRestApplication.class, args);
+	}
+
+}
---
+++ sample7_jpa_over_rest/src/test/java/com/example/jpa_over_rest/jpa_over_rest/JpaOverRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_over_rest.jpa_over_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaOverRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
